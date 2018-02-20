5ee6fa4c3882184c2ecef513db49aa132efa9e9a
Jeff Vega <jeff@intellibliss.com>
2018-02-20 12:44:14 -0800

    repeat:successful

---
+++ higherOrder.js
@@ -0,0 +1,16 @@
+// function repeat(fn,num){
+// for(let i=0;i<num;i++){
+// fn()    
+// }
+
+// }
+// repeat(hello,5)
+
+// function hello(){
+//     console.log("hello world");
+// }
+// function goodbye(){
+//     console.log("goodbye world");
+// }
+
+// repeat(goodbye,5)
